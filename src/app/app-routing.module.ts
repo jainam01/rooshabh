c8dc22f83cbe569c479473d56ca3ae1a087d6ecb
jainam01 <89692451+jainam01@users.noreply.github.com>
2022-06-16 11:55:19 +0530

    new project tailwind css install

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './header/aboutus/aboutus.component';
+import { HomeComponent } from './header/home/home.component';
+import { BabyComponent } from './header/kids/baby/baby.component';
+import { BoysComponent } from './header/kids/boys/boys.component';
+import { GirlsComponent } from './header/kids/girls/girls.component';
+import { KidsComponent } from './header/kids/kids.component';
+import { MENComponent } from './header/men/men.component';
+import { WOMENComponent } from './header/women/women.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'kids', component: KidsComponent },
+  { path: 'men', component: MENComponent },
+  { path: 'women', component: WOMENComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'boy', component: BoysComponent },
+  { path: 'girl', component: GirlsComponent },
+  { path: 'baby', component: BabyComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './header/home/home.component';
+import { MENComponent } from './header/men/men.component';
+import { WOMENComponent } from './header/women/women.component';
+import { KidsComponent } from './header/kids/kids.component';
+import { AboutusComponent } from './header/aboutus/aboutus.component';
+import { BoysComponent } from './header/kids/boys/boys.component';
+import { GirlsComponent } from './header/kids/girls/girls.component';
+import { BabyComponent } from './header/kids/baby/baby.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MENComponent,
+    WOMENComponent,
+    KidsComponent,
+    AboutusComponent,
+    BoysComponent,
+    GirlsComponent,
+    BabyComponent
   ],
   imports: [
     BrowserModule,
