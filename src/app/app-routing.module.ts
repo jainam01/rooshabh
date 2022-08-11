4f6ba79e4673d776114caff17aef85c797fada63
jainam01 <89692451+jainam01@users.noreply.github.com>
2022-08-11 10:09:25 +0530

    ADD BOXERBRIEF PART

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './header/home/home.component';
+
+// routing of innerware
 import { BoxerbriefComponent } from './header/MENFILE/INNERWEAR/boxerbrief/boxerbrief.component';
+import { TrunkComponent } from './header/MENFILE/INNERWEAR/trunk/trunk.component';
+import { InnerbriefComponent } from './header/MENFILE/INNERWEAR/innerbrief/innerbrief.component';
+import { BriefComponent } from './header/MENFILE/INNERWEAR/brief/brief.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'boxerbrief' , component:BoxerbriefComponent}
+  { path: '', component: HomeComponent },  
+
+  // routes of innerware
+  { path: 'boxerbrief' , component:BoxerbriefComponent},
+  { path: 'trunk' , component:TrunkComponent},
+  { path: 'innerbrief' , component:InnerbriefComponent},
+  { path: 'brief' , component:BriefComponent},
 ];
 
 @NgModule({
